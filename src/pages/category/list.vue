89c4771d8b95eff7e5e87a8a04e780ae5e11f7c4
= <=>
2019-10-27 18:58:10 +0800

    上传栏目瓜管理 ，基本实现搜索、添加、删除、修改、批量删除

---
+++ src/pages/category/list.vue
@@ -0,0 +1,235 @@
+<template>
+    <div class="category">
+        
+         <!-- 添加、批量删除按钮，及搜索框 -->
+        <div class="btns">
+            <el-row>
+                <el-col :span="6"> 
+                    <!-- 添加 -->
+                    <el-button type="primary" size="small" plain @click="toAddHandler">添加</el-button>
+                <!-- 批量删除 -->
+                    <el-button type="danger" size="small" plain @click="batchDeleteHandler(ids)">批量删除</el-button>
+                </el-col>
+                <!-- 搜索 -->
+                <!-- <el-col :span="8">
+                    <el-form :inline="true">
+                        <el-form-item>
+                            <el-input @keyup.enter.native="searchHandler" placeholder="请输入关键字" v-model="input" class="input-with-select">
+                                <el-button @click="searchHandler" slot="append" icon="el-icon-search"></el-button>
+                            </el-input>
+                        </el-form-item>
+                    </el-form>
+                </el-col> -->
+                <el-col :span="8" :offset="10">
+                    <el-form :inline="true" class="search" size="small" @submit.native.prevent>
+                    <el-form-item style="width:80px;">
+                        <el-select v-model="typeTag" placeholder="类型">
+                        <el-option label="栏目名称" value="name" />
+                        <el-option label="序号" value="num" />
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item style="width:180px;">
+                        <el-input
+                        v-if="typeTag == 'name'"
+                        v-model="name"
+                        placeholder="请输入栏目名称"
+                        @keyup.enter.native="loadData"
+                        >
+                        <i slot="prefix" class="el-input__icon el-icon-search" />
+                        </el-input>
+                        <el-input
+                        v-else
+                        v-model="num"
+                        placeholder="请输入序号"
+                        @keyup.enter.native="loadData"
+                        >
+                        <i slot="prefix" class="el-input__icon el-icon-search" />
+                        </el-input>
+                    
+
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button @click.prevent="loadData">搜索</el-button>
+                    </el-form-item>
+                    </el-form>
+                </el-col>
+            </el-row>
+        </div>
+
+        <!-- 表格数据 -->
+        <el-table :data="categories.list" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="55"></el-table-column>
+            <el-table-column prop="name" label="栏目名称"></el-table-column>
+            <el-table-column prop="num" label="序号"></el-table-column>
+            <el-table-column prop="icon" label="图标">
+                <template v-slot:default="record">
+                    <img :src="record.row.icon" alt="">
+                </template>
+            </el-table-column>
+            <!-- <el-table-column prop="parentId" label="父ID"></el-table-column> -->
+            <el-table-column #default="record" label="
+            操作">
+                <el-row>
+                    <el-button size="mini" icon="el-icon-edit" @click.prevent="editHandler(record.row)"></el-button>
+                    <el-button size="mini" icon="el-icon-delete" @click.prevent="deleteHandler(record.row.id)"></el-button>
+                </el-row>
+            </el-table-column>
+        </el-table>
+        <!-- 分页 -->
+        <el-pagination small layout="prev, pager, next" 
+            @current-change="pageChange"
+            :page-size="categories.pageSize" 
+            :total="categories.total"
+            :current-page="params.page+1"></el-pagination>
+
+        <!-- 模态框开始 -->
+        <el-dialog :title="title" :visible="visible" @close="dialogCloseHandler">
+            <el-form :model="category" :rules="rules" ref="categoryForm">
+                <el-form-item label="栏目名称" label-width="100px" prop="name">
+                    <el-input v-model="category.name"></el-input>
+                </el-form-item>
+                <el-form-item label="序号" label-width="100px" prop="num">
+                    <el-input v-model="category.num"></el-input>
+                </el-form-item>
+                <el-form-item label="图标" label-width="100px">
+                    <el-input v-model="category.icon"></el-input>
+                </el-form-item>
+                <el-form-item label="父栏目" label-width="100px" prop="parentId">
+                    <el-select v-model="category.parentId" placeholder="请选择...">
+                        <el-option v-for="c in categories.list" :label="c.name" :value="c.id" :key="c.id"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button size="small" @click="dialogCloseHandler">取消</el-button>
+                <el-button type="primary" size="small" @click="submitHandler">保存</el-button>
+            </div>
+        </el-dialog>
+        <!-- 模态框结束 -->
+    </div>
+</template>
+
+<script>
+
+import {mapState,mapGetters,mapMutations,mapActions} from "vuex"
+
+export default {
+    data(){
+        return{
+            // 存放表单
+            category:{},
+            // 多选id获取
+            ids:[],
+            // 存放搜索框内容
+            name:'',
+            num:'',
+            typeTag:'',
+            // form:{},
+            // 表单验证
+            rules:{
+                name:[
+                    {required:true,message:'请输入栏目名称',trigger:'blur'},
+                    {min:2,max:10,message:'请输入2个以上字符',trigger:'blur'}
+                ],
+                num:[
+                    {required:true,message:'请输入序号',trigger:'blur'},
+                    {min:2,max:10,message:'请输入2个以上字符',trigger:'blur'}
+                ],
+                parentId:[
+                    {required:true,message:'请选择父栏目',trigger:'blur'},
+                ]
+            }
+        }
+    },
+    computed:{
+        ...mapState("category",["categories","visible","title","params"])
+    },
+    created(){
+        this.loadData();
+        // this.findAllCategories();
+    },
+    methods:{
+        ...mapMutations("category",["showModal","closeModal","searchByName","searchByNum","setTitle"]),
+        ...mapActions("category",["findAllCategories","loadCategoryData","saveOrUpdateCategory","deleteCategoryById","batchDeleteCategory"]),
+        // 多选
+        handleSelectionChange(val){
+            this.ids = val.map(item => item.id);
+        },
+        // 分页
+        pageChange(currentPage){
+        this.params.page = currentPage-1;
+        this.loadData();
+        },
+        loadData(){
+            // 判断是否根据栏目名称或序号进行查询
+            if(this.name){
+                this.searchByName(this.name)
+            }else if(this.num){
+                this.searchByNum(this.num)
+            }
+            this.loadCategoryData();
+        },
+        // 添加
+        toAddHandler(){
+            // 重置表单
+            this.category={};
+            this.setTitle="添加顾客信息";
+            // 显示模态框
+            this.showModal();
+        },
+        // 关闭模态框
+        dialogCloseHandler(){
+            this.category = {};
+            this.closeModal();
+        },
+        // 修改
+        editHandler(row){
+            this.category = row;
+            this.setTitle="修改顾客信息";
+            this.showModal();
+        },
+        // 删除
+        deleteHandler(id){
+            this.deleteCategoryById(id)
+            .then((response)=>{
+                this.$message({
+                    type:"suceess",
+                    message:response.statusText
+                });
+            })
+        },
+        // 批量删除
+        batchDeleteHandler(ids){
+            this.batchDeleteCategory(ids)
+            .then((response)=>{
+                this.$message({
+                    type:"suceess",
+                    message:response.statusText
+                });
+            })
+        },
+        // 提交
+        submitHandler(){
+            // 表单验证
+            this.$refs.categoryForm.validate((valid)=>{
+                // 如果校验通过
+                if(valid){
+                    let promise = this.saveOrUpdateCategory(this.category)
+                    promise.then((response)=>{
+                        this.$message({
+                            type:"suceess",
+                            message:response.statusText
+                        });
+                    })
+                }else{
+                    return false;
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -1,10 +1,18 @@
-import request from '@/utils/request'
+// import request from '@/utils/request'
+import {get,post,post_array} from "@/http/axios";
 export default {
   namespaced:true,
   state:{
     visible:false,
     categories:[],
-    title:"添加栏目信息"
+    // ids:[],
+    title:"添加栏目信息",
+    params:{
+      page:0,
+      pageSize:5,
+      name:'',
+      num:''
+    }
   },
   getters:{
     countCategories:(state)=>{
@@ -22,50 +30,77 @@ export default {
     }
   },
   mutations:{
+    // 显示模态框
     showModal(state){
       state.visible = true;
     },
+    // 关闭模态框
     closeModal(state){
       state.visible = false;
     },
     // 需要接受一个参数，这个参数就是categories
     refreshCategories(state,categories){
-      console.log('state->',state);
+      console.log('state->',state.categories);
       state.categories = categories;
     },
+    // 模态框标题
     setTitle(state,title){
       state.title = title;
+    },
+    // 根据栏目名称查询
+    searchByName(state,name){
+      state.params.name = name;
+      state.params.num = ''
+    },
+    // 根据序号查询
+    searchByNum(state,num){
+      state.params.num = num;
+      state.params.name = ''
     }
   },
+
   actions:{
-    async batchDeleteCategories(context,ids){
-      let response = await request.post("/category/batchDelete",{ids});
-      context.dispatch("findAllCategories")
-      return response;
-    },
-    // async findAllCategories({commit,dispatch,getters,state}){
+    // 查询栏目信息
     async findAllCategories(context){
-      console.log("context->",context);
+      // console.log("context->",context);
       // 1. 查询所有栏目信息
-      let response = await request.get("/category/findAll");
+      let response = await get("/category/findAll");
       // 2. 将栏目信息设置到state.categories中
       context.commit("refreshCategories",response.data)
     },
+    
+    async loadCategoryData({state,commit}){
+      let response = await post("/category/query",state.params);
+      // state.categories = response.data;
+      // alert(response.data);
+      commit('refreshCategories',response.data);
+    },
+
+    // 根据id删除
     async deleteCategoryById({dispatch},id){
       // 1. 删除栏目信息
-      let response = await request.get("/category/deleteById?id="+id);
+      let response = await get("/category/deleteById",{id});
       // 2. 刷新
-      dispatch("findAllCategories")
+      dispatch("loadCategoryData")
       // 3. 提示成功
       return response;
     },
+
+    // 批量删除
+    async batchDeleteCategory({dispatch},ids){
+      let response = await post_array("/category/batchDelete",ids);
+      dispatch("loadCategoryData")
+      return response;
+    },
+
+    // 提交保存
     async saveOrUpdateCategory({dispatch,commit},category){
       // 1. 提交请求
-      let response = await request.post("/category/saveOrUpdate",category)
+      let response = await post("/category/saveOrUpdate",category)
       // 2. 关闭模态
       commit("closeModal");
       // 3. 刷新页面
-      dispatch("findAllCategories");
+      dispatch("loadCategoryData");
       // 4. 提示成功 react
       return response;
     }
